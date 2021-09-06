41bc8a5b5e6d8ba6db763aa442456942c7ad789a
Toropolski_Bartosz <toropolski.bartosz@gmail.com>
2021-09-06 13:01:37 +0200

    first commit

---
+++ Models_ML/moduls/load_data.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class DataMenager:   
+    def __init__(self, data_path, data_url=None):
+        self.data_path = data_path
+        self.data_url = data_url
+        
+    def fetch_data_from_web(self, name_data):
+        '''
+        Metoda odpowiadająca za pobranie zbioru danych
+        z repozytorum universytetu calofornijeskego
+        '''
+        try:
+            if not os.path.isdir(self.data_path + '/raw'):
+                os.makedirs(self.data_path + '/raw')
+            df = pd.read_csv(self.data_url, header = None)    
+            df.to_csv(f'{self.data_path}/raw/{name_data}',index=False)
+        except:
+            print('Error')
+            
+    def load_raw_data(self, name_data, columns=None):
+        try:
+            df = pd.read_csv(f'{self.data_path}/raw/{name_data}')
+            if columns:
+                df.columns = columns
+            return df
+        except:
+            print('Dane nie mogły zostac załadowane ')
+    
+    def uploade_data(self, dataframe, name_data):
+        '''
+        Metoda służąca do szybiekgo zapisania danych
+        do katalogu ustawionego przy wywołaniu klasy '''
+        try:
+            if not os.path.isdir(self.data_path + '/preprocessing'):
+                os.makedirs(self.data_path + '/preprocessing')
+                
+            dataframe.to_csv(f'{self.data_path}/preprocessing/{name_data}',index=False)
+        except:
+            print('Dane nie zostały zapisane')    
+    
+    def load_preprocessing_data(self, name_data):
+        try:
+            df = pd.read_csv(f'{self.data_path}/preprocessing/{name_data}')
+            return df
+        except:
+            print('Dane nie mogły zostac załadowane ')
